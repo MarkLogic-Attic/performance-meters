e6873060167748e5af20a613099c8b2cc2fbff57
Michael Blakeley <michael.blakeley@marklogic.com>
2010-08-31 13:13:22 -0700

    remove duplicate

--- publish.sh
+++ publish.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #
 
+set -e
+
 ant jar \
-    && rsync -vaP lib/performance-meters.jar /home/mblakele/lib/java/ \
     && rsync -vaP lib/performance-meters.jar ../performance-meters-gh-pages/ \
     && rsync -vaP lib/performance-meters.jar $HOME/lib/java/ \
     && rsync -vazP lib/performance-meters.jar ssh.marklogic.com:lib/java/
+
+date
